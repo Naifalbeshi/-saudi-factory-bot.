3c04c5173362dcc935703db25061bc5572567b97
Naifalbeshi <naifalbeshi@hotmail.com>
2026-09-25 23:35:55 +0300

    Update factory_bot.py

--- factory_bot.py
+++ factory_bot.py
@@ -44,10 +44,3 @@ def webhook():
 
 if __name__ == "__main__":
     server.run(host="0.0.0.0", port=int(os.environ.get('PORT', 5000)))
-
-@server.route("/")
-def webhook():
-    return "بوت مصنع معين الخير يعمل بنجاح!", 200
-
-if __name__ == "__main__":
-    server.run(host="0.0.0.0", port=int(os.environ.get('PORT', 5000)))
